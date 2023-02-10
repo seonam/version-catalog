bfe3d9442333d1a757588a6ef43a1792f622dad8
disdong123 <gksrlfw@gmail.com>
2023-02-11 03:09:23 +0900

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    `version-catalog`
+    `maven-publish`
+    signing
+}
+
+group = "kr.disdong.version.catalog"
+version = "0.0.1"
+
+catalog {
+    versionCatalog {
+        from(files("libs.version.toml"))
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("gpr") {
+            groupId = project.group as String
+            artifactId = "catalog"
+            version = project.version as String
+            from(components["versionCatalog"])
+        }
+    }
+
+    repositories {
+        maven {
+            name = "GithubPackages"
+            url = uri("https://maven.pkg.github.com/disdong123/version-catalog")
+            credentials {
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("GITHUB_USERNAME")
+                password = project.findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+}
